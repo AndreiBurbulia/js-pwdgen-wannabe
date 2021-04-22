6fd8e4303c9a962a955bb7ad63b3499064fb6ae3
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-22 15:43:45 +0200

    Esercizio Bonus Inventato

---
+++ Bonus/assets/js/main.js
@@ -0,0 +1,29 @@
+
+
+//Chiedo nome utente
+var nomeUtente = prompt("Inserisci il tuo Nome!! \nAttenzione verrà tenuto conto delle minuscole e delle MAIUSCOLE !!!")
+
+//chiedo cognome utente
+var cognomeUtente = prompt("Inserisci il tuo Cognome!! \nAttenzione verrà tenuto conto delle minuscole e delle MAIUSCOLE !!!")
+
+//colore preferito
+var coloreUtente = prompt("Inserisci il tuo colore preferito!! \nAttenzione verrà tenuto conto delle minuscole e delle MAIUSCOLE !!!")
+
+// chiedo un numero
+var numeroUtente = prompt("Inserisci un numero da usare!!")
+
+//Do il benvenuto al cliente
+document.getElementById("titolo").innerHTML = "Benvenuto " + nomeUtente
+
+//stampo prima opzione di password
+document.getElementById("prima_scelta").innerHTML = nomeUtente + cognomeUtente + coloreUtente + numeroUtente
+
+//stampo seconda opzione di password
+document.getElementById("seconda_scelta").innerHTML = cognomeUtente + coloreUtente + numeroUtente + nomeUtente
+
+//stampo terza opzione di password
+document.getElementById("terza_scelta").innerHTML = coloreUtente + numeroUtente + nomeUtente + cognomeUtente
+
+//stampo quarta opzione di password
+document.getElementById("quarta_scelta").innerHTML = numeroUtente + cognomeUtente + coloreUtente + nomeUtente
+
